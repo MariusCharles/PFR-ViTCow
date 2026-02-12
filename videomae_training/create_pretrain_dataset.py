778f91371da1daba7d279b202bf1c0aba7cbb2a4
Charlotte Tibi <charlotte.tibi14@gmail.com>
2026-02-12 14:53:51 +0000

    Automatically get frame numbers

--- videomae_training/create_pretrain_dataset.py
+++ videomae_training/create_pretrain_dataset.py
@@ -1,13 +1,42 @@
 import os
-from config import NUM_FRAMES_PER_CLIP, PRETRAIN_DIR
-from pipeline.cloud import download_sftp_pretrain_dataset
+from config import PRETRAIN_DIR
+from cloud import download_sftp_pretrain_dataset
 import argparse
 from typing import List, Dict
+import subprocess
+import json
 
-def build_videomae_list(video_paths: List[str], out_file: str):
+from tqdm import tqdm
+from typing import Iterable, Optional
+
+def progress_bar(iterable: Iterable,
+                 desc: Optional[str] = None):
+    return tqdm(
+        iterable,
+        desc=desc,
+        unit="video",
+        ncols=100,
+        leave=True
+    )
+
+def get_num_frames(video_path: str) -> int:
+    cmd = [
+        "ffprobe",
+        "-v", "error",
+        "-select_streams", "v:0",
+        "-count_frames",
+        "-show_entries", "stream=nb_read_frames",
+        "-of", "json",
+        video_path,
+    ]
+    result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    data = json.loads(result.stdout)
+    return int(data["streams"][0]["nb_read_frames"])
+
+def build_videomae_list(video_paths: List[str], out_file: str, desc: str):
     with open(out_file, "w") as f:
-        for path in video_paths:
-            n_frames = NUM_FRAMES_PER_CLIP
+        for path in progress_bar(video_paths, desc=desc):
+            n_frames = get_num_frames(path)
             f.write(f"{path} {n_frames}\n")
 
 def get_train_test_local_paths() -> Dict[str, List[str]]:
@@ -42,6 +71,8 @@ if __name__ == "__main__":
         dataset = get_train_test_local_paths()
 
     build_videomae_list(video_paths=dataset["train"], 
-                        out_file=f"{PRETRAIN_DIR}/train.csv")
+                        out_file=f"{PRETRAIN_DIR}/train.csv",
+                        desc="Train csv creation")
     build_videomae_list(video_paths=dataset["test"], 
-                        out_file=f"{PRETRAIN_DIR}/test.csv")
+                        out_file=f"{PRETRAIN_DIR}/test.csv",
+                        desc="Test csv creation")
